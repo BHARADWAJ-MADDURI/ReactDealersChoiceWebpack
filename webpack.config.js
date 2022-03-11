5bdeb765c611f86ab9e2530d927a13b9efc76f68
BHARADWAJ-MADDURI <bmadduri19@gmail.com>
2022-03-10 20:55:59 -0600

    webpack updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  devtool: 'source-map',
     module: {
       rules: [
         {
